63924b65c6a28d8128970061d13e2f7ff4a79cd8
khalilliu <lkhaliliu@gmail.com>
2021-12-04 13:21:32 +0800

    finish 04

--- 03/p1.cpp
+++ 03/p1.cpp
@@ -5,24 +5,24 @@
 using namespace std;
 
 const int N = 2010;
-int w[N];
+string s[N];
 int n;
 
-void solve1()
-{
-  n = 0;
-  int res = 0;
-  while(cin >> w[n]) {
-    if(n > 0) {
-      res += (w[n] > w[n-1]);
-    }
-    n++;
-  }
-  cout << res << endl;
-
-}
-
 int main() {
-  solve1();
+  while(cin >> s[n]) {n++;}
+  int m = s[0].size();
+  int a = 0, b = 0;
+  // for(int i=0; i<n; i++) cout << s[i] << endl;
+  for(int i=0; i<m; i++) {
+    int t0 = 0, t1 = 0;
+    for(int j=0; j<n; j++) {
+      int t = s[j][i] - '0';
+      if(t == 1) t1++;
+      else t0++;
+    }
+    a = (a << 1) + (t1 > t0);
+    b = (b << 1) + (t1 < t0);
+  }
+  cout << a * b << endl;
   return 0;
 }
--- 03/p2.cpp
+++ 03/p2.cpp
@@ -5,21 +5,52 @@
 using namespace std;
 
 const int N = 2010;
-int w[N];
-int n, res, sum, pre;
+string s[N];
+int cnt[2][20], tmp[2][20];
+bool st[N];
+int n, m;
 
+int solve(int u) {
+  memcpy(tmp, cnt, sizeof cnt);
+  memset(st, 0, sizeof st);
+  int tot = n, res = 0, t = 0;
+  for(int i=0; i<m; i++) {
+    if(u) t = tmp[1][i] >= tmp[0][i] ? 1 : 0;
+    else t = tmp[0][i] <= tmp[1][i] ? 0 : 1;
+    for(int j=0; j<n; j++) {
+      if(s[j][i] - '0' != t && !st[j]) {
+        st[j] = true;
+        tot--;
+        for(int k=i; k<m; k++) {
+          tmp[s[j][k] - '0'][k]--;
+        }
+      }
+    }
+    if(tot == 1) {
+      break;
+    }
+  }
+  for(int i=0; i<n; i++) {
+    if(!st[i]) {
+      for(int j=0; j<m; j++) {
+        res = (res << 1) + (s[i][j] - '0');
+      }
+      break;
+    }
+  }
+  return res;
+}
 
 int main() {
-  n = 0, sum = 0, pre = 0;
-  while(cin >> w[n]) {
-    pre = sum;
-    sum += w[n];
-    if(n >= 3) {
-      sum -= w[n-3];
-      if(sum > pre) res++;
+  while(cin >> s[n]) {n++;}
+  m = s[0].size();
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m; j++) {
+      cnt[s[i][j] - '0'][j]++;
     }
-    n++;
   }
-  cout << res << endl;
+  int a = solve(1), b = solve(0);
+  cout << a << ' ' << b << endl;
+  cout << a * b << endl;
   return 0;
 }
