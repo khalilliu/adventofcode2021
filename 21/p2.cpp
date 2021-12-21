bf526994436cbaf41af9029c28f8a07776776534
khalilliu <lkhaliliu@gmail.com>
2021-12-21 15:28:28 +0800

    refactor 21

--- 21/p2.cpp
+++ 21/p2.cpp
@@ -4,23 +4,24 @@
 #include <map>
 
 using namespace std;
-
 typedef long long LL;
 typedef pair<LL, LL> PLL;
-LL p1, p2;
-map<pair<PLL, PLL>, PLL> f;
 
-PLL dp(LL p1, LL p2, LL st1, LL st2) {
+const int N = 30;
+LL p1, p2;
+PLL f[N][N][N][N];
+
+PLL dp(int p1, int p2, int st1, int st2) {
     if(st1 >= 21) return {1, 0};
     if(st2 >= 21) return {0, 1};
-    if(f.count({{p1, p2}, {st1, st2}})) return f[{{p1, p2}, {st1, st2}}];
+    if(f[p1][p2][st1][st2] != PLL{0, 0}) return f[p1][p2][st1][st2];
     
     PLL ans = {0, 0};
     for(int d1=1; d1<=3; d1++){
         for(int d2=1; d2<=3; d2++){
             for(int d3=1; d3<=3; d3++){
-                LL new_p1 = (p1 + d1 + d2 + d3)%10;
-                LL new_st1 = new_p1 + st1 + 1;
+                int new_p1 = (p1 + d1 + d2 + d3)%10;
+                int new_st1 = new_p1 + st1 + 1;
                 
                 auto t = dp(p2, new_p1, st2, new_st1);
                 ans = {ans.first + t.second, ans.second + t.first};
@@ -28,8 +29,7 @@ PLL dp(LL p1, LL p2, LL st1, LL st2) {
         }
     }
     
-    f[{{p1, p2}, {st1, st2}}] = ans;
-    return ans;
+    return f[p1][p2][st1][st2] = ans;
 }
 
 int main() {
@@ -38,7 +38,6 @@ int main() {
     p1 = s1[s1.size()-1] - '0', p2 = s2[s2.size()-1] - '0';
 
     p1--, p2--;
-    
 
     auto t = dp(p1, p2, 0, 0);
 
