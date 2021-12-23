07788f046d6845e828daee61769f195dcaccf48d
khalilliu <lkhaliliu@gmail.com>
2021-12-23 16:09:07 +0800

    finish 23

---
+++ 23/p1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+const int N = 15;
+
+
+int main() {
+    // no code
+    // i solve this problem by hand
+    return 0;
+}
+
+
+
---
+++ 23/p2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+
+    // no code !!!
+    // solve this problem by hand !
+    return 0;
+}
